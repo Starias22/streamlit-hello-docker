31e5326485d4ddfd78d2ae7fcbd31db596023edd
Starias22 <adedeezechiel@gmail.com>
2024-08-22 02:37:53 +0100

    .dockerignore, app.py, and build config added

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("Hello, Streamlit!")
+    #st.title("Hello, Streamlit!- Version2")
+    st.write("Build strategy: Docker")
+
+
+if __name__ == "__main__":
+    main()
